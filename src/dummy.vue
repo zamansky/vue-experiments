0b1d4c5ff722b2f3c18cccd6711324eea5ca3028
Mike Zamansky <zamansky@gmail.com>
2017-08-21 13:22:53 -0400

    Z

---
+++ src/dummy.vue
@@ -0,0 +1,28 @@
+<template>
+<h1>Dummy</h1>
+</template>
+
+
+<script>
+
+ export default {
+     name :'',
+     data : function(){
+	 return {
+	     
+	 }
+     },
+     methods: {
+
+
+     },
+     components:{
+
+     }
+ }
+ 
+</script>
+
+<style scoped>
+
+</style>
